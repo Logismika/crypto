6df13bb83ba48a23e7cbf30ebee592039897f62e
Dmitry Bublik <dmitry.buba@gmail.com>
2024-10-02 16:32:30 +0300

    make expandKey()

--- src/utils/kuznechik/expandKey.ts
+++ src/utils/kuznechik/expandKey.ts
@@ -1,25 +1,25 @@
 import { createHash } from "crypto";
-import { KEY_SIZE } from "./consts";
+import { BLOCK_SIZE, KEY_SIZE, l_vec, Pi } from "./consts";
 import { ExpandedKey } from "./ExpandedKey";
 import { byte, vect } from "./types";
 import { toArray } from "../buffer-helper";
 import * as R from "ramda";
-import { createVect } from "./utils";
+import { createBytes, toByte } from "./utils";
 
-export const expandKey = (key: string | byte[]) =>
+export const expandKey = (key: string | vect) =>
     typeof key === "string" ? expandKeyString(key) : expandKeyArray(key);
 
 const expandKeyString = (key: string): ExpandedKey =>
     expandKeyArray(toArray<byte>(createHash("sha256").update(key).digest()));
 
-const expandKeyArray = (key: byte[]): ExpandedKey => {
+const expandKeyArray = (key: vect): ExpandedKey => {
     if (key.length !== KEY_SIZE) {
         throw new Error(`Key's length must be ${KEY_SIZE}.`);
     }
 
     const result: ExpandedKey = {
-        iter_c: R.range(0, 32).map(() => createVect()),
-        iter_key: R.range(0, 10).map(() => createVect()),
+        iter_c: R.range(0, 32).map(() => createBytes(BLOCK_SIZE)),
+        iter_key: R.range(0, 10).map(() => createBytes(BLOCK_SIZE)),
     }
 
     const key_1 = key.slice(KEY_SIZE / 2);
@@ -30,8 +30,8 @@ const expandKeyArray = (key: byte[]): ExpandedKey => {
     result.iter_key[1] = key_2.slice();
     let iter_1 = key_1.slice();
     let iter_2 = key_2.slice();
-    let iter_3: byte[];
-    let iter_4: byte[];
+    let iter_3: vect;
+    let iter_4: vect;
     for (let i = 0; i < 4; i += 1) {
         const retVal1 = GOST_Kuz_F(iter_1, iter_2, result.iter_c[0 + 8 * i]); iter_3 = retVal1.out_key_1; iter_4 = retVal1.out_key_2;
         const retVal2 = GOST_Kuz_F(iter_3, iter_4, result.iter_c[1 + 8 * i]); iter_1 = retVal2.out_key_1; iter_2 = retVal2.out_key_2;
@@ -50,22 +50,90 @@ const expandKeyArray = (key: byte[]): ExpandedKey => {
 }
 
 const GOST_Kuz_Get_C = (iter_c: vect[]) => {
-    const iter_num = R.range(0, 32).map(() => createVect()), ;
+    const iter_num = R.range(0, 32).map(() => createBytes(BLOCK_SIZE));
 
     for (let i = 0; i < iter_num.length; i++) {
-        iter_num[i][0] = (i + 1) as byte;
+        iter_num[i][0] = toByte(i + 1);
     }
+
     for (let i = 0; i < iter_c.length; i++) {
-        GOST_Kuz_L(iter_num[i], iter_c[i]);
+        iter_c[i] = GOST_Kuz_L(iter_num[i]);
     }
 }
 
-const GOST_Kuz_F = (in_key_1: byte[], in_key_2: byte[], iter_const: byte[]): { out_key_1: byte[], out_key_2: byte[] } => {
+const GOST_Kuz_F = (in_key_1: vect, in_key_2: vect, iter_const: vect): { out_key_1: vect, out_key_2: vect } => {
     const out_key_2 = in_key_1.slice();
-    const intrl1 = GOST_Kuz_X(in in_key_1, in iter_const);
-    const intrl2 = GOST_Kuz_S(in intrl1);
-    const intrl3 = GOST_Kuz_L(in intrl2);
-    const out_key_1 = GOST_Kuz_X(in intrl3, in in_key_2);
+    const intrl1 = GOST_Kuz_X(in_key_1, iter_const);
+    const intrl2 = GOST_Kuz_S(intrl1);
+    const intrl3 = GOST_Kuz_L(intrl2);
+    const out_key_1 = GOST_Kuz_X(intrl3, in_key_2);
 
     return { out_key_1, out_key_2 }
+}
+
+const GOST_Kuz_L = (in_data: vect): vect => {
+    let result: vect = in_data.slice();
+    for (let i = 0; i < 16; i += 1) {
+        result = GOST_Kuz_R(result);
+    }
+    return result;
+}
+
+const GOST_Kuz_R = (in_data: vect): vect => {
+    let a_15 = 0;
+    const internal: vect = createBytes(16);
+    for (let i = 15; i >= 0; i -= 1) {
+        if (i > 0) {
+            internal[i - 1] = in_data[i];
+        }
+
+        a_15 ^= GOST_Kuz_GF_mul(in_data[i], l_vec[i]);
+    }
+
+    internal[15] = toByte(a_15);
+    return internal;
+}
+
+const GOST_Kuz_GF_mul = (in_a: byte, in_b: byte): byte => {
+    let a = in_a;
+    let b = in_b;
+
+    let c = 0;
+    for (let i = 0; i < 8; i += 1)
+    {
+        if ((b & 1) != 0)
+        {
+            c ^= a;
+        }
+        const hi_bit = a & 0x80;
+        a <<= 1;
+        if (hi_bit != 0)
+        {
+            a ^= 0xc3; //полином x^8+x^7+x^6+x+1
+        }
+        b >>= 1;
+    }
+    return toByte(c);
+}
+
+const GOST_Kuz_X = (a: vect, b: vect): vect => {
+    const c = createBytes(BLOCK_SIZE);
+
+    for (let i = 0; i < c.length; i += 1)
+    {
+        c[i] = toByte(a[i] ^ b[i]);
+    }
+
+    return c;
+}
+
+const GOST_Kuz_S = (in_data: vect): vect => {
+    const out_data = createBytes(BLOCK_SIZE);
+
+    for (let i = 0; i < out_data.length; i += 1)
+    {
+        out_data[i] = Pi[in_data[i]];
+    }
+
+    return out_data;
 }
--- src/utils/kuznechik/utils.ts
+++ src/utils/kuznechik/utils.ts
@@ -1,5 +1,12 @@
 import * as R from "ramda";
-import { BLOCK_SIZE } from "./consts";
 import { byte, vect } from "./types";
 
-export const createVect = (): vect => R.range(0, BLOCK_SIZE).map(() => 0 as byte);
+export const createBytes = (count: number): vect => R.range(0, count).map(() => toByte(0));
+
+export const toByte = (value: number): byte => {
+    if (value < 0 || value >= 256) {
+        throw new RangeError(`Byte value out of range. Value = ${value}`);
+    }
+
+    return value as byte;
+}
